66073a475b6843fe1446fee07d54e89ff72c9ed1
Ferran Celades <ferrancelades@hotmail.es>
2016-11-24 16:54:35 +0100

    Asserts, pre and postcondition added to Title

--- src/reports/Title.java
+++ src/reports/Title.java
@@ -7,18 +7,28 @@ public class Title extends ReportElement {
 	 */
 	private String text;
 	
+	private boolean invariant(){
+		if (this.text == null){
+			return false;
+		}
+		return true;
+	}
+	
 	public Title(final String textValue) {
-		assert textValue != null;
+		assert textValue != null: "Title can't be null";
 		this.text = textValue;
+		assert invariant();
 	}
 	
 	@Override
     public final void accept(final ReportVisitor visitor) {
 		assert visitor != null;
 		visitor.visitTitle(this);
+		assert invariant();
 	}
 	
 	public final String getText() {
+		assert invariant();
 		return this.text;
 	}
 	
